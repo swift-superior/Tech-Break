ee472dbf72169af1ef6c79a137e2bcafcd99f6c4
swift-superior <lee_yu_hang@s2023.ssts.edu.sg>
2024-06-26 22:03:37 +0800

    Initial Commit

---
+++ Tech Break/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tech Break
+//
+//  Created by Jerro Lee on 26/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tech Break/Tech_BreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_BreakApp.swift
+//  Tech Break
+//
+//  Created by Jerro Lee on 26/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Tech_BreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
