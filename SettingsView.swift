f82f6767f7907bb63295fa9465da454d93ba399a
swift-superior <lee_yu_hang@s2023.ssts.edu.sg>
2024-06-26 22:18:40 +0800

    Added basic TabView in ContentView, and SettingsView (settings).

---
+++ SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Tech Break
+//
+//  Created by Jerro Lee on 26/6/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
